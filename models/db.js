f31b5d7a04b169da9fbc34cc17049e30cef0337f
unknown <ritosisida@gmail.com>
2026-06-02 20:23:03 +0900

    MySQL 코드를 몽고DB 코드로 완전 교체

--- models/db.js
+++ models/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-// .env 파일에 저장한 세팅 주소로 연결
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://root:1234@cluster0.ye5gfbq.mongodb.net/?appName=Cluster0";
+// 하드코딩된 주소 유지 (확실한 방법)
+const MONGO_URI = "mongodb+srv://root:knutweb0506@cluster0.ye5gfbq.mongodb.net/mymarket?appName=Cluster0";
 
 mongoose.connect(MONGO_URI)
-  .then(() => console.log('🍃 MongoDB 연결 성공!'))
-  .catch(err => console.error('MongoDB 연결 실패:', err));
+  .then(() => console.log('🍃 MongoDB Atlas 연결 대성공!!!'))
+  .catch(err => console.error('🚨 MongoDB 연결 실패:', err));
 
 module.exports = mongoose;
